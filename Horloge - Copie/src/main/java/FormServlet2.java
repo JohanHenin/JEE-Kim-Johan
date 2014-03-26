323f68009fe7b94926699d3f35dd5316b813fb79
JohanHenin <johan.henin@gmail.com>
2014-03-26 17:06:28 +0100

    fin exercice formulaire avec session
    test des expressions regulieres sur le code postal

--- Horloge - Copie/src/main/java/FormServlet2.java
+++ Horloge - Copie/src/main/java/FormServlet2.java
@@ -1,7 +1,7 @@
 
 
 import java.io.IOException;
-
+import java.util.regex.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,14 +14,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/FormServlet2")
 public class FormServlet2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FormServlet2() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public FormServlet2() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
@@ -33,8 +33,25 @@ public class FormServlet2 extends HttpServlet {
 		String postalCode = request.getParameter("postalCode");
 		String name = (String) request.getSession().getAttribute("userName");
 		String age = (String) request.getSession().getAttribute("userAge");
-		
-		response.getWriter().write(street + city + postalCode + name + age);
+		if (checkAddress(street,city,postalCode)){
+			response.getWriter().write("Nom: " + name + "\n" +
+					"Age: " + age + " ans" + "\n" +
+					street + " " + postalCode + " " + city);
+		}
+		else{
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Une ou plusieurs information(s) est/sont mal renseignee(s)");
+		}
+
+	}
+
+	private boolean checkAddress(String street, String city, String postalCode){
+		boolean AddressOk = false;
+		Pattern pattern = Pattern.compile("[0-9]{5}");
+		Matcher matcher = pattern.matcher(postalCode);
+		if (matcher.matches() && !"".equals(street) && !"".equals(city)){
+			AddressOk = true;
+		}
+		return AddressOk;
 	}
 
 }
