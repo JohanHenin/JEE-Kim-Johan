d9c3b7d282010f32a42b084b83aaca9165b91b78
JohanHenin <johan.henin@gmail.com>
2014-04-23 10:44:09 +0200

    mockito update mais non fini

--- inscription web-app/inscription/src/test/java/fr/epsi/mvc/TestInscriptionServletMockito.java
+++ inscription web-app/inscription/src/test/java/fr/epsi/mvc/TestInscriptionServletMockito.java
@@ -1,28 +1,40 @@
 package fr.epsi.mvc;
 
-import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.junit.Before;
 import org.junit.Test;
+
 import static org.mockito.Mockito.*;
 
 
-public class TestInscriptionServletMockito {
+public class TestInscriptionServletMockito 
+{
+	HttpServletRequest request;
+	HttpServletResponse response;
+	
+	InscriptionServlet inscriptionServlet;
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		request = mock(HttpServletRequest.class); 
+		response = mock(HttpServletResponse.class);
+		
+		inscriptionServlet = new InscriptionServlet();
+	}
+	
 	 @Test
-	  public void testDemoMockito() throws Exception {
-		    HttpServletRequest request = mock(HttpServletRequest.class);       
-	        HttpServletResponse response = mock(HttpServletResponse.class);    
-
+	  public void testDemoMockito() throws Exception 
+	  {
 	        when(request.getParameter("login")).thenReturn("me");
-	        when(request.getParameter("email")).thenReturn("aaaa@aaa.aaa");
+	        when(request.getParameter("email")).thenReturn("testemail@gmail.com");
 	        when(request.getParameter("conditionsGeneralesApprouvees")).thenReturn("true");
 
-	        new InscriptionServlet().doPost(request, response);
+	        inscriptionServlet.doPost(request, response);
 
 	        assertNotNull(request.getAttribute("inscription"));
 	  }
